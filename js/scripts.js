2843d6a052f0ee3c4efa591c6cc96f02969d117e
Brandon <name.brandon.aguero@gmail.com>
2023-09-04 19:45:19 -0500

    Se terminaron todas las funcionalidades del proyecto

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 const buttonMenuHam = document.querySelector(".title-button__button");
+const iconMenu = document.querySelector(".title-button__icon");
 const header = document.querySelector(".header");
 
 buttonMenuHam.addEventListener("click", () => {
   header.classList.toggle("header--active");
+  iconMenu.classList.toggle("bx-rotate-90");
 });
